89658b85e71e25da3648955d34e9494f0add0ab2
iifeofadarkrose <122921193+iifeofadarkrose@users.noreply.github.com>
2023-07-17 15:47:43 +0200

    fix

--- send_mail.php
+++ send_mail.php
@@ -1,37 +1,44 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
+require "PHPMailer/src/Exception.php";
+require "PHPMailer/src/PHPMailer.php";
 
-    require "PHPMailer/src/Exception.php";
-    require "PHPMailer/src/PHPMailer.php";
+$mail = new PHPMailer(true);
 
-    $mail = new PHPMailer(true);
-	
-    $mail->CharSet = "UTF-8";
-    $mail->IsHTML(true);
+$mail->CharSet = "UTF-8";
+$mail->IsHTML(true);
 
-   
-    $email = $_POST["email"];
-	$email_template = "template_mail.html";
+$email = $_POST["email"];
+$email_template = "template_mail.html";
 
-    $body = file_get_contents($email_template);
-	$body = str_replace('%email%', $email, $body);
+$body = file_get_contents($email_template);
+$body = str_replace('%email%', $email, $body);
 
-
-    $mail->addAddress("vacheslav69moss@gmail.com");   // Здесь введите Email, куда отправлять
-	$mail->setFrom($email);
-    $mail->Subject = "[Form request]";
-    $mail->MsgHTML($body);
+$mail->addAddress("vacheslav69moss@gmail.com");
+$mail->setFrom($email);
+$mail->Subject = "[Form request]";
+$mail->MsgHTML($body);
 
 if (!$mail->send()) {
     $message = 'Ошибка';
-  } else {
+} else {
     $message = 'Данные отправлены!';
-  }
+    // Отправка автоматического ответа пользователю
+    $autoResponse = new PHPMailer(true);
+    $autoResponse->CharSet = "UTF-8";
+    $autoResponse->IsHTML(true);
+    $autoResponse->addAddress($email); // Адрес, на который отправляется автоответ
+    $autoResponse->setFrom("your_email@example.com"); // Твой адрес электронной почты
+    $autoResponse->Subject = "thank you for your request!";
+    $autoResponse->Body = "Thank you for submitting your email address! Here is your free tutorial: https://www.youtube.com/watch?v=fV1er2csjro&ab_channel=Creaconelb%C3%BAho";
+    $autoResponse->send();
+}
 
-  $response = ['message' => $message];
+$response = ['message' => $message];
 
-  header('location: index.html');
-  echo json_encode($response);
+header('location: index.html');
+echo json_encode($response);
+
 ?>
